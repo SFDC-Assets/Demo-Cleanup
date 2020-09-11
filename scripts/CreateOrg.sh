557645c37b14197109a640c92284815e49513dce
John Meyer <john.meyer@johnmeyer-ltm2.internal.salesforce.com>
2020-09-11 15:35:55 -0400

    Added package.json, time zone setting in scratch org creation

--- scripts/CreateOrg.sh
+++ scripts/CreateOrg.sh
@@ -3,7 +3,8 @@ readonly orgAlias="DemoCleanupScratch"
 readonly devHubUserName="MyComponents"
 
 echo "*** Creating scratch org ..."
-sfdx force:org:create --definitionfile config/project-scratch-def.json \
+sfdx force:org:create \
+    --definitionfile config/project-scratch-def.json \
     --type scratch \
     --nonamespace \
     --targetdevhubusername "$devHubUserName" \
@@ -17,6 +18,8 @@ echo "*** Assigning permission set to your user ..."
 sfdx force:user:permset:assign --permsetname Demo_Cleanup --loglevel error
 echo "*** Generating password for your user ..."
 sfdx force:user:password:generate --targetusername "$orgAlias" --loglevel error
+echo "*** Setting time zone for your user ..."
+sfdx force:data:record:update --sobjecttype User --where "Name='User User'" --values "TimeZoneSidKey='America/New_York'" --loglevel error
 echo "*** Creating sample data ..."
 sfdx force:apex:execute --apexcodefile "scripts/apex/DemoCleanupTasks.apex" --targetusername "$orgAlias" --loglevel error
 sfdx force:apex:execute --apexcodefile "scripts/apex/SampleData.apex" --targetusername "$orgAlias" --loglevel error
